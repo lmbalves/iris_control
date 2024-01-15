97e20c802919e205f1a5e58542e74ff641e1b400
tugalinebacker <msilva.cmiguel@gmail.com>
2024-01-15 00:23:20 +0000

    Add first thruster control logic based on highest scoring box detection

--- src/setpoints_visual_navigation.cpp
+++ src/setpoints_visual_navigation.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
 #include <ros/ros.h>
 #include <std_msgs/Float32MultiArray.h>
+#include "setpoints_visual_navigation.hpp"
 
-void centerPointsCallback(const std_msgs::Float32MultiArray::ConstPtr& centerPointsMsg){
-    std::vector<float> data = centerPointsMsg->data;
+void VisualNavigation::imageCenterPointsCallback(const std_msgs::Float32MultiArray::ConstPtr& centerPointsMsg){
+    m_imageCenterPointsData = centerPointsMsg->data;
     std::cout << "\n--------------";    
-    std::cout << "\nCenter frame = (" << data[0] << "," << data[1] << ")";    
-    std::cout << "\nCenter box = (" << data[2] << "," << data[3] << ")";    
-    std::cout << "\n--------------\n";    
+    std::cout << "\nCenter frame = (" << m_imageCenterPointsData[0] << "," << m_imageCenterPointsData[1] << ")";    
+    std::cout << "\nCenter box = (" << m_imageCenterPointsData[2] << "," << m_imageCenterPointsData[3] << ")";    
+    std::cout << "\n--------------\n";  
+    thrusterControl(m_imageCenterPointsData);
 }
+
+void VisualNavigation::thrusterControl(std::vector<float> imageCenterPoints){
+    /* Calculate error in X and Y*/
+    m_xError = imageCenterPoints[0]-imageCenterPoints[2];
+    m_yError = imageCenterPoints[1]-imageCenterPoints[3];
+
+    /* thruster input values = constant*error */
+    m_thrusterControlX = m_xConstant*m_xError;
+    m_thrusterControlY = m_yConstant*m_yError;
+
+    /* Normalize thruster input values*/
+    m_thrusterControlXN = (m_thrusterControlX/imageCenterPoints[0]);
+    m_thrusterControlYN = (m_thrusterControlY/imageCenterPoints[1]);
+
+    std::cout << "\n|||||||||||||||||";
+    std::cout << "\nThruster control = (" << m_thrusterControlXN << "," << m_thrusterControlYN << ")";    
+    std::cout << "\n|||||||||||||||||";
+
+    /* HEAVE */
+    m_setpoints[4] = m_thrusterControlYN;
+    m_setpoints[5] = m_thrusterControlYN;
+    /* YAW */
+    m_setpoints[6] = m_thrusterControlXN;
+    m_setpoints[7] = m_thrusterControlXN;
+
+    std_msgs::Float64MultiArray msgSetpoints;
+    msgSetpoints.data = m_setpoints;
+    m_pubThrusters.publish(msgSetpoints);
+}
+
 
 int main (int argc, char **argv){
     ros::init(argc, argv, "setpoints_visual_navigation");
     
     ros::NodeHandle nh;
-    ros::Subscriber subCenterPoints = nh.subscribe("/iris/proscilica_front/image_center_points", 1000, centerPointsCallback);
+    VisualNavigation imageCenterPoints = VisualNavigation(&nh);
     ros::spin();
 }
---
+++ src/setpoints_visual_navigation.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <ros/ros.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Float64MultiArray.h>
+
+class VisualNavigation
+{
+public:
+    VisualNavigation(ros::NodeHandle *nh)
+    {
+        m_subCenterPoints = nh->subscribe("/iris/proscilica_front/image_center_points",
+                                          1000, &VisualNavigation::imageCenterPointsCallback, this);
+        m_pubThrusters = nh->advertise<std_msgs::Float64MultiArray>("/iris/controller/thruster_setpoints", 1000);
+    }
+    /**
+     * @brief Callback method that receives a vector containing the center point of the camera image 
+     * and the center point of the highest scoring bounding box.
+     */
+    void imageCenterPointsCallback(const std_msgs::Float32MultiArray::ConstPtr &centerPointsMsg);
+    
+    /**
+     * @brief Method that generates normalized inputs to the thrusters.
+     */
+    void thrusterControl(std::vector<float> imageCenterPoints);
+
+private:
+    ros::Subscriber m_subCenterPoints;
+    ros::Publisher m_pubThrusters;
+    std::vector<float> m_imageCenterPointsData;
+    std::vector<double> m_setpoints = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+    float m_xError, m_yError;
+    float m_thrusterControlX, m_thrusterControlY;
+    float m_thrusterControlXN, m_thrusterControlYN;
+    float m_xConstant = 0.1;
+    float m_yConstant = 0.5;
+};
