52366956bbb90e87dbaaa071462f1f39bcdb82f5
lmbalves <lmbalves@gmail.com>
2024-01-21 19:00:56 +0000

    keeping the files for LASMU project delivery

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup
  
 setup(
     version='0.0.0',
-    scripts=['src/path_motor_control2.py','src/control_vision.py','src/control_rotors.py'],
+    scripts=['src/plot_path.py'],
     packages=['iris_control'],
-    package_dir={'': 'src'}
-)
+    package_dir={'': 'src'})
